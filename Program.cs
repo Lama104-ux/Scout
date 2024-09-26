6c2686d798236f9684724678adf3b56c44b8dfcc
Gus <gusgit@fastmail.org>
2024-09-26 23:55:13 +0200

    Removed solution

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿ScoutRepository repo = new();
-
-//Fyll på med några scouter och aktiviteter från början (för att vi inte alltid måste lägga in saker för att testa)
-repo.AddScout(new Scout("Sam", "sam@scouterna.se", "1234567890", new DateOnly(2004, 1, 1)));
-repo.AddScout(new Scout("Kim", "kim@scouterna.se", "1234567890", new DateOnly(2009, 10, 1)));
-repo.AddScout(new Scout("Alex", "alex@scouterna.se", "1234567890", new DateOnly(2010, 1, 12)));
-repo.AddActivity(new Activity("Hajk", new DateTime(2021, 10, 1, 14, 0, 0), "Vi ska ut och vandra i skogen och tälja en pinne."));
-
-Menu menu = new(repo);
-menu.Run();
+﻿
--- Scout.cs
+++
@@ -1,7 +0,0 @@
-public class Scout(string name, string email, string phone, DateOnly birthdate)
-{
-    public string Name { get; set; } = name;
-    public string Email { get; set; } = email;
-    public string Phone { get; set; } = phone;
-    public DateOnly Birthdate { get; set; } = birthdate;
-}
