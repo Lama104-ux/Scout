d6fe1d6783b5120c60e01dc4ad05cfbe9e7761b7
Gus <gusgit@fastmail.org>
2024-10-02 22:43:49 +0200

    Add age

--- Scout.cs
+++ Scout.cs
@@ -5,4 +5,11 @@ class Scout
     public string Name { get; set; }
     public string Email { get; set; }
     public DateOnly DateOfBirth { get; set; }
+    public int Age
+    {
+        get
+        {
+            return (int)((DateTime.Now - DateOfBirth.ToDateTime(new TimeOnly(12, 0))).TotalDays / 365.25);
+        }
+    }
 }
