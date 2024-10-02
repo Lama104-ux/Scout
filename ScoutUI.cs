e3041765956add451143bdc4fa5d2713e2fedf08
Gus <gusgit@fastmail.org>
2024-10-02 15:53:31 +0200

    Start with UI

--- ScoutUI.cs
+++ ScoutUI.cs
@@ -14,10 +14,65 @@ class ScoutUI
     {
         while (true)
         {
-
+            Console.Clear();
+            PrintMenu();
+            MakeChoice(Console.ReadLine().ToUpper());
         }
     }
 
+    private void PrintMenu()
+    {
+        Console.WriteLine("GÖR DITT VAL:");
+        Console.WriteLine("A: Registrera scout");
+        Console.WriteLine("B: Visa scoutregister");
+        Console.WriteLine("Q: Avsluta");
+    }
 
+    private void MakeChoice(string input)
+    {
+        if (input == "A")
+        {
+            RegisterScout();
+        }
+        if (input == "B")
+        {
+            ShowAllScouts();
+        }
+        if (input == "Q")
+        {
+            Environment.Exit(0);
+        }
+    }
 
+    private void RegisterScout()
+    {
+        Console.Clear();
+        string name = GetString("Ange scoutens namn:");
+        string email = GetString("Ange scoutens e-post:");
+        _repo.AddScout(new Scout(name, email));
+        Console.WriteLine("Scouten är registrerad!");
+        Console.ReadKey();
+    }
+
+    private void ShowAllScouts()
+    {
+        Console.Clear();
+        Console.WriteLine("ALLA SCOUTER:");
+        //foreach (var scout in _repo.GetAllScouts())
+        Console.ReadKey();
+    }
+
+    private string GetString(string prompt = "")
+    {
+        do
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+            Console.WriteLine("Du måste ange något!");
+        } while (true);
+    }
 }
